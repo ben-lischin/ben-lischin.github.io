b63cc5393256d15912c478f3de122153472a8ca4
Ben Lischin <benlischin@.com>
2024-04-25 11:53:21 -0400

    final project changes

--- script.js
+++ script.js
@@ -41,13 +41,16 @@ function openLink(url) {
 // project info
 const projectNames = document.querySelectorAll('.projectName');
 const projectView = document.querySelector('.projectView');
-const projectSection = document.getElementById('projects');
 const projectInfos = [
     { 
         id: 'project1',
         content: `
             <div class="projectContent">
                 <h2>Covey.Town: Shogi</h2>
+                <div class="projectLinks">
+                    <a href="https://github.com/ben-lischin/covey.town-shogi" target="_blank">GitHub Repo</a>
+                    <a href="https://shogi-301.onrender.com/" target="_blank">Live Deployment</a>
+                </div>
                 <div class="projectDescription">
                     <p>This extension of the original Covey.Town codebase enables users to play and spectate real-time single or multiplayer games of Shogi. We provide various informational and assistive features to help you figure out the game. When a match starts, each player is given a 10-minute move timer which ticks down during their turn, forfeiting the game at 0. While waiting for another user to join, players have the option to begin a game against our Shogi engine. This engine is implemented using the Negamax algorithm, with adjustable search depth for scalable difficulty settings.</p>
                     <p>Firebase was integrated to support user authentication and record persistence across sessions, feeding the town leaderboard with all active player stats.</p>
@@ -79,10 +82,14 @@ const projectInfos = [
         content: `
             <div class="projectContent">
                 <h2>Vestio</h2>
+                <div class="projectLinks">
+                    <a href="https://github.com/ben-lischin/Vestio" target="_blank">GitHub Repo</a>
+                    <a href="https://docs.google.com/presentation/d/1-mSYY5tT8vgVl54vgSB63c7F3QVsmnR19aIyS-rEFJA/edit?usp=sharing" target="_blank">Demo</a>
+                </div>
                 <div class="projectDescription">
-                    <p>Vestio is a live trading app for iOS that allows email-authenticated users to invest fake currency and track their portfolio. Enabling users to practice trading in this no-risk environment, Vestio aims to give users confidence in their investment strategies.</p>
+                    <p>Vestio is a live trading app for iOS that allows email-authenticated users to invest fake currency and track their portfolio. Enabling users to practice trading in this no-risk environment, we aim to give users confidence in their investment strategies.</p>
                     <p>Users start with $1,000 to invest - they can search for companies, maintain a watchlist, and buy/sell shares to grow their wealth. At any point, the user can reset their account.</p>
-                    <p>Current stock data and market prices were maintained in-app using the Alpha Vantage API.</p>
+                    <p>Current market data and stock prices are maintained in-app using the Alpha Vantage API.</p>
                 </div>
                 <div class="projectSkills">
                     <div class="projectSkill">Swift</div>
@@ -97,9 +104,12 @@ const projectInfos = [
         content: `
             <div class="projectContent">
                 <h2>UltraHealth</h2>
+                <div class="projectLinks">
+                    <a href="https://github.com/ben-lischin/UltraHealth" target="_blank">GitHub Repo</a>
+                </div>
                 <div class="projectDescription">
                     <p>This project is a medical web app designed to facilitate seamless interaction and data-transfer between 3 archetypal medical-space users: patients, doctors, pharmacists. A simple front-end was built using Appsmith.</p>
-                    <p>The back-end includes an original normalized relational database containing 9 tables, 75+ attributes, and 500+ rows of sample user data. We also deployed a REST API with a Python Flask container in Docker, grant each user unique role-based CRUD operations.</p>
+                    <p>The back-end includes an original normalized relational database containing 9 tables, 75+ attributes, and 500+ rows of sample user data. We also deployed a REST API with a Python Flask container in Docker, granting each user unique role-based CRUD operations.</p>
                 </div>
                 <div class="projectSkills">
                     <div class="projectSkill">Python</div>
@@ -116,6 +126,9 @@ const projectInfos = [
         content: `
             <div class="projectContent">
                 <h2>Image Editor</h2>
+                <div class="projectLinks">
+                    <a href="https://github.com/ben-lischin/ImageEditor" target="_blank">GitHub Repo</a>
+                </div>
                 <div class="projectDescription">
                     <p>With this processor, users may load, edit, and save images of multiple file types (JPG, JPEG, PNG, PPM, BMP, etc).</p>
                     <p>Implemented model-view-controller design pattern to support user interaction through a Swing GUI application. Alternatively, if specified through command-line arguments, the user can either pass in a script of commands or type into the terminal directly to edit an image.</p>
@@ -135,14 +148,17 @@ const projectInfos = [
         content: `
             <div class="projectContent">
                 <h2>Maze Solver</h2>
+                <div class="projectLinks">
+                    <a href="https://github.com/ben-lischin/MazeSolver" target="_blank">GitHub Repo</a>
+                </div>
                 <div class="projectDescription">
                     <p>The project interface loads with random maze of specified or default dimension. This maze is built by applying Kruskal's algorighm to generate a minimum spanning tree of a (seeded) randomly weighted graph of all the cells.</p>
                     <p>Users may intercat to visualize a breadth-first or depth-first search from the start cell to the end cell. The program utilizes a backtracking algorithm that, upon completing a search, highlights the most efficient path. At any point, users may refresh with a new maze to try solving.</p>
                 </div>
                 <div class="projectSkills">
                     <div class="projectSkill">Java</div>
-                    <div class="projectSkill">JUnit</div>
                     <div class="projectSkill">Graphs</div>
+                    <div class="projectSkill">JUnit</div>
                 </div>
             </div>
         `
