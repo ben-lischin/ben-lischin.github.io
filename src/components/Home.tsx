9258386a7f67dcc1b9959c935d822599e402b359
Ben Lischin <benlischin@gmail.com>
2025-02-18 03:22:36 -0500

    typewriter animation

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,18 +1,36 @@
+import { TypeAnimation } from 'react-type-animation';
+
 const Home = () => {
     return (
         <section id="home">
             <div className="home">
-               <h1>Ben Lischin</h1>
-               <hr />
-               <p className="description">Software Engineer | Student</p>
-               <div className="links">
-                  <a href="https://www.linkedin.com/in/ben-lischin/" target="_blank">
-                     <img src="/res/linkedin.png" alt="Linkedin" />
-                   </a>
-                   <a href="https://github.com/ben-lischin" target="_blank">
-                     <img src="/res/github.png" alt="GitHub" />
-                   </a>
-               </div>
+                <h1>Ben Lischin</h1>
+                <hr />
+                <p className="description">
+                    <TypeAnimation
+                        cursor={false}
+                        className="cursor-animation"
+                        speed={{type: 'keyStrokeDelayInMs', value: 75}}
+                        sequence={[
+                            1000,
+                            'Software Engineer',
+                            200,
+                            'Software Engineer | Student',
+                            3000,
+                            (el) => {
+                                el?.classList.remove("cursor-animation");
+                            }
+                        ]}
+                    />
+                </p>
+                <div className="links">
+                        <a href="https://www.linkedin.com/in/ben-lischin/" target="_blank">
+                            <img src="/res/linkedin.png" alt="Linkedin" />
+                        </a>
+                        <a href="https://github.com/ben-lischin" target="_blank">
+                            <img src="/res/github.png" alt="GitHub" />
+                        </a>
+                </div>
             </div>
         </section>
     );
