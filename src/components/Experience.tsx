1eb71d74f5bd1bed2c07b4c1789d30b9b835f433
Ben Lischin <benlischin@gmail.com>
2026-04-20 21:55:55 -0400

    exp transition

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -32,11 +32,13 @@ const Experience = () => {
                                 {!expand3 && 
                                     <span> ...</span>
                                 }
-                                {expand3 && 
-                                    <ul>
-                                        <li>Software Engineer on Email Platforms.</li>
-                                    </ul>
-                                }
+                                <div className={`desc-wrapper ${expand3 ? "open" : ""}`}>
+                                    <div>
+                                        <ul>
+                                            <li>Software Engineer on Email Platforms.</li>
+                                        </ul>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                   </div>
@@ -54,14 +56,16 @@ const Experience = () => {
                                 {!expand2 && 
                                     <span> ...</span>
                                 }
-                                {expand2 && 
-                                    <ul>
-                                        <li>Created a dashboard providing visibility into Klaviyo's email sending metrics – designing a data collection system as well as building a centralized dashboard to present the information.</li>
-                                        <li>Implemented thread-safe metric collection with goroutines, able to push data to DynamoDB and flush memory on interval. This was integrated directly into our batch event processing service, handling high-volume production traffic. Configured cron jobs to clean records and maintain historical rollups.</li>
-                                        <li>Developed a REST API with Go and Gin, exposing the database via internal ingress secured to the company VPC as well as specified IP ranges.</li>
-                                        <li>Designed a near real-time React interface to highlight these key metrics, including data visualizations and paginated tables, and support historical data downloads. The dashboard is regulated by user authentication with role-based access to the particular page.</li>
-                                    </ul>
-                                }
+                                <div className={`desc-wrapper ${expand2 ? "open" : ""}`}>
+                                    <div>
+                                        <ul>
+                                            <li>Created a dashboard providing visibility into Klaviyo's email sending metrics – designing a data collection system as well as building a centralized dashboard to present the information.</li>
+                                            <li>Implemented thread-safe metric collection with goroutines, able to push data to DynamoDB and flush memory on interval. This was integrated directly into our batch event processing service, handling high-volume production traffic. Configured cron jobs to clean records and maintain historical rollups.</li>
+                                            <li>Developed a REST API with Go and Gin, exposing the database via internal ingress secured to the company VPC as well as specified IP ranges.</li>
+                                            <li>Designed a near real-time React interface to highlight these key metrics, including data visualizations and paginated tables, and support historical data downloads. The dashboard is regulated by user authentication with role-based access to the particular page.</li>
+                                        </ul>
+                                    </div>
+                                </div>
                               </div>
                         </div>
                   </div>
@@ -84,14 +88,16 @@ const Experience = () => {
                                 onClick={() => setExpand1(!expand1)}
                             />
                             {!expand1 && <span> ...</span>}
-                            {expand1 && 
-                                <ul>
-                                    <li>Completed POC and scoped work for a new PII redaction service used to protect sensitive user information in 150M+ emails processed daily.</li>
-                                    <li>Enhanced e2e automation testing efficiency for the DemandTools app by upgrading the Cypress test suite; optimized existing test scripts for improved reliability and maintainability, and added new coverage following TDD.</li>
-                                    <li>Built a SenderScore POC data pipeline to assess the deliverability performance of 2M+ IPs in a daily rollup of S3 email data. Senders receive a score gauging their reputation as well as a summary report to inform them on where they may have been penalized. I implemented optimized clustering, normalization, and vector similarity computations to restrict deliverability comparisons to only senders of relative volume.</li>
-                                    <li>Developed an Angular frontend for a new internal tool to help identify and maintain Product Qualified Leads.</li>
-                                </ul>
-                            }
+                            <div className={`desc-wrapper ${expand1 ? "open" : ""}`}>
+                                <div>
+                                    <ul>
+                                        <li>Completed POC and scoped work for a new PII redaction service used to protect sensitive user information in 150M+ emails processed daily.</li>
+                                        <li>Enhanced e2e automation testing efficiency for the DemandTools app by upgrading the Cypress test suite; optimized existing test scripts for improved reliability and maintainability, and added new coverage following TDD.</li>
+                                        <li>Built a SenderScore POC data pipeline to assess the deliverability performance of 2M+ IPs in a daily rollup of S3 email data. Senders receive a score gauging their reputation as well as a summary report to inform them on where they may have been penalized. I implemented optimized clustering, normalization, and vector similarity computations to restrict deliverability comparisons to only senders of relative volume.</li>
+                                        <li>Developed an Angular frontend for a new internal tool to help identify and maintain Product Qualified Leads.</li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
                    </div>
                 </div>
